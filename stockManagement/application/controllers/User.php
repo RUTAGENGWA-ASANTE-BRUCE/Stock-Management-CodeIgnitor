880cb385659127fa8cc233608f3610ebfb53ed75
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-18 13:09:41 +0200

    new change

---
+++ stockManagement/application/controllers/User.php
@@ -0,0 +1,13 @@
+<?php
+class User extends CI_Controller {
+    function createUser(){
+        $this->load->view('createUser');
+    }
+    function login(){
+
+    }
+    function updateUser(){
+
+    }
+}
+?>
