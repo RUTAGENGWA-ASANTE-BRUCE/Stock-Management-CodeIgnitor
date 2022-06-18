6ba6367b1ac1386a1e14a06e62b1fa1d89f7b724
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-18 20:20:19 +0200

    new change

--- stockManagement/application/views/createUser.php
+++ stockManagement/application/views/createUser.php
@@ -6,16 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Stock Management - Register User</title>
     <Link rel="stylesheet"  href="<?php echo base_url();?>/public/css/bootstrap.css">
-    <Link rel="stylesheet"  href="<?php echo base_url();?>/public/css/custom.css">
+    <Link rel="stylesheet"  href="<?php echo base_url();?>/public/css/custom2.css">
     
 </head>
 <body class="loginRegister">
-        <div class="border col-md-4 d-flex flex-column   bg-white my-auto border-primary rounded-3 mx-auto mt-5" style="height:700px">
-            <div class="col-md-12   bg-primary h-20 rounded-top" style="height: 30%;;">
-            <img src="<?php echo base_url();?>/public/images/user.png" class="w-25   " style="height:150px;object-fit:contain;margin-left:35%;"/>
-            <h5 class="w-25 mx-auto text-white">Welcome</h5>
-        </div>
-            <form method="POST" name="createUser" style="height:70%;" class="col-md-12" action="<?php echo base_url()?>index.php/User/create">
+        <div class="border col-md-4 d-flex flex-column position-relative   bg-white my-auto border-primary rounded-3 mx-auto mt-5" style="height:700px">
+            <h5 class="w-25 mx-auto describer text-white mt-5">Signup</h5>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" ><path fill="#0d6efd" fill-opacity="1" d="M0,32L120,69.3C240,107,480,181,720,213.3C960,245,1200,235,1320,229.3L1440,224L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z">
+            
+            </path></svg>
+            <form method="POST" name="createUser" style="height:80%;" class="col-md-12" action="<?php echo base_url()?>index.php/User/create">
             <div class="form-part">
                 <Label>Name</Label>
                 <input type="text" name="email" class="form-input" />
@@ -31,6 +31,10 @@
             <div class="form-part">
                 <Label>Confirm Password</Label>
                 <input type="cpassword" name="password" class="form-input" />
+            </div>
+            <button class="btn btn-primary bg-primary signup">Signup</button>
+            <div class="form-part">
+                <a class="signin">Signin</a>
             </div>
             </form>
         </div>
