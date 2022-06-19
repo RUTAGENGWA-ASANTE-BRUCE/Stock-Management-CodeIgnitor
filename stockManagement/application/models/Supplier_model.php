3f516dbcc9b14ce73ac66d0a858ac7291c1799c9
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-19 19:14:02 +0200

    new change

---
+++ stockManagement/application/models/Supplier_model.php
@@ -0,0 +1,25 @@
+<?php
+    class Supplier_model extends CI_Model {
+        function create($formArray){
+            $this->db->insert("suppliers",$formArray);
+            return $user=$this->db->get('suppliers')->row_array();
+            
+        }
+        function login($formArray){
+            $this->db->where('email',$formArray['email']);
+            return $user=$this->db->get('suppliers')->row_array();
+        }
+    
+        function updateUser($user_id,$formArray){
+            $this->db->where('user_id',$user_id);
+            $this->db->update('suppliers',$formArray);
+        }
+        function getUser($user_id){
+            $this->db->where('user_id',$user_id);
+            return $user=$this->db->get('suppliers')->row_array();//select * from suppliers where user_id=?;
+           }
+           function all(){
+            return $users=$this->db->get('suppliers')->result_array();//Select * from users'
+        }
+    }
+?>
