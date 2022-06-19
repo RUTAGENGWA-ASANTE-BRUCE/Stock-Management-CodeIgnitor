4d924d7d77ba42f88b2c7acd3431a91d19d9af06
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-19 11:13:23 +0200

    new change

--- stockManagement/application/controllers/User.php
+++ stockManagement/application/controllers/User.php
@@ -1,13 +1,79 @@
 <?php
 class User extends CI_Controller {
     function createUser(){
-        $this->load->view('createUser');
+        $this->load->model('User_model');
+        $this->form_validation->set_rules('name',"Name",'required');
+        $this->form_validation->set_rules('email',"Email",'required|valid_email');
+        $this->form_validation->set_rules('password',"Password",'required');
+
+        if($this->form_validation->run()==false){
+            $this->load->view('createUser');
+        }
+        else{
+            $formArray=array();
+            $formArray['name']=$this->input->post('name');
+            $formArray['email']=$this->input->post('email');
+            $formArray['password']=hash("SHA512",$this->input->post('password'));
+            $user=$this->User_model->create($formArray);
+            $this->session->set_flashdata('success','User Registered successfully');
+            $data['user']=$user;
+            redirect(base_url()."index.php/Stock/stockProducts/".$user['user_id']);
+        }
     }
     function login(){
-        $this->load->view('login');
+        $this->load->model('User_model');
+        $this->form_validation->set_rules('email',"Email",'required|valid_email');
+        $this->form_validation->set_rules('password',"Password",'required');
+    
+        if($this->form_validation->run()==false){
+            $this->load->view('login');
+        }
+        else{
+            $formArray=array();
+            $formArray['email']=$this->input->post('email');
+            $formArray['password']=hash("SHA512",$this->input->post('password'));
+            $user=$this->User_model->login($formArray);
+            if(!$user){
+             $this->load->view('login');
+            }else{
+                $data['user']=$user;
+                $this->session->set_flashdata('success','You logged in successfully');
+                redirect(base_url()."index.php/Stock/stockProducts/".$user['user_id']);
+            }
+        }
+        
+    }
+    function userInfo($user_id){
+        $this->load->model('User_model');
+        $user= $this->User_model->getUser($user_id);
+        $data=array();
+        $data['user']=$user;
+        $this->load->view('user_info',$data);
 
     }
-    function updateUser(){
+    function edit($user_id){
+        $this->load->model('User_model');
+        $user= $this->User_model->getUser($user_id);
+        $data=array();
+        $data['user']=$user;
+        $this->form_validation->set_rules("name","Name","required");
+        $this->form_validation->set_rules("email","Email","required|valid_email");
+        $this->form_validation->set_rules('password',"Password",'required');
+        
+        if($this->form_validation->run()==false){
+            $this->load->view('editProfile',$data);
+        }
+        else{
+            $formArray=array();
+            $formArray['name']=$this->input->post('name');
+            $formArray['email']=$this->input->post('email');
+            $formArray['password']=hash("SHA512",$this->input->post('password'));
+            $this->User_model->updateUser($user_id,$formArray);
+            $this->session->set_flashdata('success',"Profile updated successfully!");
+            $this->session->set_flashdata('userInfo',$user);
+            redirect(base_url()."index.php/User/userInfo/".$user_id);
+        }
+        
 
     }
 }
