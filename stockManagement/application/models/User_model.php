f3ce142654eaec6d593a1b6190d6d106a08c4e8b
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-19 11:14:16 +0200

    new change

---
+++ stockManagement/application/models/User_model.php
@@ -0,0 +1,21 @@
+<?php
+    class User_model extends CI_Model {
+        function create($formArray){
+            $this->db->insert("users",$formArray);
+            $this->db->where($formArray);
+            return $user=$this->db->get('users')->row_array();
+        }
+        function login($formArray){
+            $this->db->where('email',$formArray['email']);
+            return $user=$this->db->get('users')->row_array();
+        }
+        function updateUser($user_id,$formArray){
+            $this->db->where('user_id',$user_id);
+            $this->db->update('users',$formArray);
+        }
+        function getUser($user_id){
+            $this->db->where('user_id',$user_id);
+            return $user=$this->db->get('users')->row_array();//select * from users where user_id=?;
+           }
+    }
+?>
