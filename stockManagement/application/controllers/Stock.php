6b221feb533ff7b7408bacb9bcec58e92a977469
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-19 11:13:57 +0200

    new change

--- stockManagement/application/controllers/Stock.php
+++ stockManagement/application/controllers/Stock.php
@@ -1,7 +1,12 @@
 <?php
 class Stock extends CI_Controller{
-    function stockProducts(){
-        $this->load->view('productsStock');
+    function stockProducts($user_id){
+        $this->load->model('User_model');
+        $user=$this->User_model->getUser($user_id);
+        $data=array();
+        $data['user']=$user;
+        
+        $this->load->view('productsStock',$data);
     }
 }
 ?>
