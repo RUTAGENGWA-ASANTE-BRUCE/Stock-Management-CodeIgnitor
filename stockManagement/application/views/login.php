7ec143ad24eb1824c56fa1b51caa990dd27fbd64
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-20 21:37:17 +0200

    new change

--- stockManagement/application/views/login.php
+++ stockManagement/application/views/login.php
@@ -15,7 +15,7 @@
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0d6efd" fill-opacity="1" d="M0,32L120,69.3C240,107,480,181,720,213.3C960,245,1200,235,1320,229.3L1440,224L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z">
             
         </path></svg>
-            <form method="POST" name="createUser" style="height:70%;" class="col-md-12" action="<?php echo base_url()?>index.php/User/login">
+            <form method="POST" name="createUser" style="height:70%;" class="col-md-12" action="<?php echo base_url()?>User/login">
            
             <div class="form-part">
                 <Label>Email</Label>
@@ -31,7 +31,7 @@
             </div>
           
             <button class="btn btn-primary bg-primary signup text">Signin</button>
-                <a class="signin btn text-white bg-dark" href="<?php echo base_url()?>index.php/User/createUser">Signup</a>
+                <a class="signin btn text-white bg-dark" href="<?php echo base_url()?>User/createUser">Signup</a>
             </form>
         </div>
         </div>
