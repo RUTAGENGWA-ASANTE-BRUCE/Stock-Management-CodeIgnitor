331955b32774d8a4c7c790f6a7216e6fda7bc213
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-20 08:04:11 +0200

    new change

--- stockManagement/application/views/createSupplier.php
+++ stockManagement/application/views/createSupplier.php
@@ -72,15 +72,15 @@
 
 <body class="loginRegister">
     <div class="border col-md-4 d-flex flex-column position-relative   bg-white my-auto border-primary rounded-3 mx-auto mt-3" style="height:760px">
-        <h5 class="w-25 mx-auto describer text-white mt-4">Signup</h5>
+        <h5 class="w-50 mx-auto describer text-white mt-4">Create Supplier</h5>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
             <path fill="#0d6efd" fill-opacity="1" d="M0,32L120,69.3C240,107,480,181,720,213.3C960,245,1200,235,1320,229.3L1440,224L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z">
 
             </path>
         </svg>
-        <form method="POST" name="createUser" enctype="multipart/form-data" style="height:80%;" class="col-md-12" action="<?php echo base_url() ?>index.php/User/createUser ">
+        <form method="POST" name="createUser" enctype="multipart/form-data" style="height:80%;" class="col-md-12" action="<?php echo base_url() ?>index.php/Stock/createSupplier/<?php echo $user['user_id'];?>">
             <div class="form-part">
-                <Label>User Name</Label>
+                <Label class="">Supplier Name</Label>
                 <input required type="text" minlength="4" maxlength="60" name="name" class="form-input" />
               <p class="fs-6 text-danger">  <?php echo form_error('name'); ?></p>
 
@@ -109,39 +109,21 @@
                 </div>
               <p class="fs-6 text-danger">  <?php echo form_error('email'); ?></p>
             </div>
-            <div class="form-part d-flex flex-column">
-                <div class="d-flex">
-
-                    <p class="col-5">Nationality</p>
-                    <select name="nationality">
-                        <option value="Rwandan">Rwandan</option>
-                        <option value="American">American</option>
-                        <option value="Ugandan">Ugandan</option>
-                        <option value="Canadian">Candadian</option>
-                    </select>
-                </div>
-              <p class="fs-6 text-danger">  <?php echo form_error('email'); ?></p>
-            </div>
-            <div class="form-part d-flex flex-column">
-                <div class="d-flex">
-
-                    <p class="col-5">Prodile Picture</p>
-                    <input required type="file" name="profilePicture">
-                </div>
-              <p class="fs-6 text-danger">  <?php echo form_error('email'); ?></p>
-            </div>
+          
             <div class="form-part">
-                <Label>Password</Label>
-                <input required type="password" pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$" name="password" class="form-input" />
-              <p class="fs-6 text-danger">  <?php echo form_error('password'); ?></p>
+                <Label>Telephone Number</Label>
+                <input required type="tel"  name="telephone" class="form-input" />
+              <p class="fs-6 text-danger">  <?php echo form_error('telephone'); ?></p>
 
             </div>
             <div class="form-part">
-                <Label>Confirm Password</Label>
-                <input required type="password" name="cpassword" class="form-input" />
+                <Label>Location</Label>
+                <input required type="text"  name="location" class="form-input" />
+              <p class="fs-6 text-danger">  <?php echo form_error('location'); ?></p>
+
             </div>
-            <button class="btn btn-primary bg-primary signup">Signup</button>
-            <a class="signin btn bg-dark text-white" href="<?php echo base_url() ?>index.php/User/login">Signin</a>
+            <button class="btn btn-primary bg-primary signup">Create Supplier</button>
+            <a class="signin btn bg-dark text-white" href="<?php echo base_url().'index.php/Stock/productSuppliers/'.$user['user_id'] ; ?>">Cancel</a>
         </form>
     </div>
     </div>
