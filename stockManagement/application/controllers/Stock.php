89988ce7dcd0a9bf175c4518242efe91378a6590
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-20 08:04:44 +0200

    new change

--- stockManagement/application/controllers/Stock.php
+++ stockManagement/application/controllers/Stock.php
@@ -1,26 +1,78 @@
 <?php
-class Stock extends CI_Controller{
-    function stockProducts($user_id){
+class Stock extends CI_Controller
+{
+    function stockProducts($user_id)
+    {
         $this->load->model('User_model');
-        $user=$this->User_model->getUser($user_id);
-        $data=array();
-        $data['user']=$user;
-        $this->load->view('productsStock',$data);
+        $user = $this->User_model->getUser($user_id);
+        $data = array();
+        $data['user'] = $user;
+        $this->load->view('productsStock', $data);
     }
-    function  productSuppliers($user_id){
+    function createProduct($user_id)
+    {
         $this->load->model('User_model');
-        $user=$this->User_model->getUser($user_id);
-        $data=array();
-        $data['user']=$user;
+        $user = $this->User_model->getUser($user_id);
+        $this->load->model('Supplier_model');
+        $suppliers=$this->Supplier_model->all();
+        $data = array();
+        $data['user'] = $user;
+        $data['suppliers'] = $suppliers;
+        $this->load->model('Supplier_model');
+        $this->form_validation->set_rules('name', "Name", 'required');
+        $this->form_validation->set_rules('category', "Category", 'required');
+        $this->form_validation->set_rules('price', "Price", 'required');
+        $this->form_validation->set_rules('color', "Color", 'required');
+        $this->form_validation->set_rules('supplier', "Supplier", 'required');
+        $this->form_validation->set_rules('brand', "Brand", 'required');
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('createProduct', $data);
+        } else {
+            $formArray = array();
+            $formArray['name'] = $this->input->post('name');
+            $formArray['category'] = $this->input->post('category');
+            $formArray['price'] = $this->input->post('price');
+            $formArray['color'] = $this->input->post('color');
+            $formArray['supplier'] = $this->input->post('supplier');
+
+            $config =[
+                'allowed_types' => 'gif|jpg|png|jpeg',
+                'upload_path'    => 'public/images'
+            ];
+            $config['encrypt_name'] = TRUE;
+            $this->load->library('upload', $config);
+            $this->upload->initialize($config);
+            if ($this->upload->do_upload('productPicture')) {
+                $formArray['profilePicture'] = $this->upload->data('file_name');
+            } else {
+                $this->Supplier_model->create($formArray);
+                $this->session->set_flashdata('success', 'Supplier Registered successfully');
+                redirect(base_url() . "index.php/Stock/stockProducts/" . $user['user_id']);
+            }
+
+        
+        }
+    }
+
+    function  productSuppliers($user_id)
+    {
+        $this->load->model('User_model');
+        $user = $this->User_model->getUser($user_id);
+        $data = array();
+        $data['user'] = $user;
+        $this->load->model('Supplier_model');
+        $suppliers = $this->Supplier_model->all();
+        $data['suppliers'] = $suppliers;
         $this->load->view('productSuppliers', $data);
     }
 
     function createSupplier($user_id)
-    {   
+    {
         $this->load->model('User_model');
-        $user=$this->User_model->getUser($user_id);
-        $data=array();
-        $data['user']=$user;
+        $user = $this->User_model->getUser($user_id);
+        $data = array();
+        $data['user'] = $user;
         $this->load->model('Supplier_model');
         $this->form_validation->set_rules('name', "Name", 'required');
         $this->form_validation->set_rules('email', "Email", 'required|valid_email');
@@ -29,7 +81,7 @@ class Stock extends CI_Controller{
         $this->form_validation->set_rules('location', "location", 'required');
 
         if ($this->form_validation->run() == false) {
-            $this->load->view('createSupplier',$data);
+            $this->load->view('createSupplier', $data);
         } else {
             $formArray = array();
             $formArray['name'] = $this->input->post('name');
@@ -39,8 +91,8 @@ class Stock extends CI_Controller{
             $formArray['gender'] = $this->input->post('gender');
 
             $this->Supplier_model->create($formArray);
-            $this->session->set_flashdata('success', 'User Registered successfully');
-            redirect(base_url() . "index.php/Stock/productSuppliers/".$user['user_id']);
+            $this->session->set_flashdata('success', 'Supplier Registered successfully');
+            redirect(base_url() . "index.php/Stock/productSuppliers/" . $user['user_id']);
         }
     }
 
@@ -67,5 +119,7 @@ class Stock extends CI_Controller{
             redirect(base_url() . "index.php/User/userInfo/" . $user_id);
         }
     }
+
+    
+
 }
-?>
