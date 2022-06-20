55c9267fc3c7fb7ccfeb9723383d62851b951db1
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-20 17:54:45 +0200

    new change

--- stockManagement/application/models/Inventory_model.php
+++ stockManagement/application/models/Inventory_model.php
@@ -5,16 +5,20 @@
             return $inventory=$this->db->get('inventories')->row_array();
             
         }
-        function updateProduct($user_id,$formArray){
-            $this->db->where('user_id',$user_id);
+        function updateInventory($inventory_id,$formArray){
+            $this->db->where('inventory_id',$inventory_id);
             $this->db->update('inventories',$formArray);
         }
-        function getProduct($user_id){
-            $this->db->where('user_id',$user_id);
+        function getInventory($inventory_id){
+            $this->db->where('inventory_id',$inventory_id);
             return $inventory=$this->db->get('inventories')->row_array();//select * from inventory where inventory_id=?;
            }
            function all(){
             return $inventories=$this->db->get('inventories')->result_array();//Select * from inventory'
+        }
+        function delete($inventory_id){
+            $this->db->where('inventory_id',$inventory_id);
+            $this->db->delete('inventories');
         }
     }
 ?>
