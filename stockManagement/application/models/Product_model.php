23fdba5af718c1f9c01b282724ed6f8e37bd134f
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-20 17:55:10 +0200

    new change

--- stockManagement/application/models/Product_model.php
+++ stockManagement/application/models/Product_model.php
@@ -5,16 +5,20 @@
             return $user=$this->db->get('products')->row_array();
             
         }
-        function updateProduct($user_id,$formArray){
-            $this->db->where('user_id',$user_id);
+        function updateProduct($product_id,$formArray){
+            $this->db->where('product_id',$product_id);
             $this->db->update('products',$formArray);
         }
-        function getProduct($user_id){
-            $this->db->where('user_id',$user_id);
+        function getProduct($product_id){
+            $this->db->where('product_id',$product_id);
             return $product=$this->db->get('products')->row_array();//select * from products where user_id=?;
            }
            function all(){
             return $products=$this->db->get('products')->result_array();//Select * from users'
+        }
+        function delete($product_id){
+            $this->db->where('product_id',$product_id);
+            $this->db->delete('products');
         }
     }
 ?>
