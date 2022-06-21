3be6cbd40614b39798601b2ae99186352f42386e
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-21 14:30:51 +0200

    new change

--- stockManagement/application/views/editProfile.php
+++ stockManagement/application/views/editProfile.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<?php if (!isset($SESSION['user_data'])){
+        header(base_url().'/User/login');
+    }
+    ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,7 +19,7 @@
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" ><path fill="#0d6efd" fill-opacity="1" d="M0,32L120,69.3C240,107,480,181,720,213.3C960,245,1200,235,1320,229.3L1440,224L1440,0L1320,0C1200,0,960,0,720,0C480,0,240,0,120,0L0,0Z">
             
             </path></svg>
-            <form method="POST" name="createUser" style="height:80%;" class="col-md-12"  action="<?php echo base_url().'user/edit/'.$user['user_id'];?>">
+            <form method="POST" name="createUser" style="height:80%;" class="col-md-12"  action="<?php echo base_url().'user/edit';?>">
             <div class="form-part">
                 <Label>Name</Label>
                 <input type="text" name="name" class="form-input"  value="<?php echo set_value('name',$user['name']);?>" />
