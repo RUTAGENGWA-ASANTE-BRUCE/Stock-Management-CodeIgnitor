06a31cf7e3725b4a08b2773fed79a69a70749017
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-21 14:30:59 +0200

    new change

--- stockManagement/application/views/createUser.php
+++ stockManagement/application/views/createUser.php
@@ -2,6 +2,10 @@
 <html lang="en">
 
 <head>
+<?php if (isset($SESSION['user_data'])){
+        header(base_url().'/Stock/stockProducts');
+    }
+    ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
