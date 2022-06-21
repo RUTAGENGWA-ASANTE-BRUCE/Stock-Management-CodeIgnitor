bb9e282fd108c90d2076c1f82dc0a65f69b32990
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-21 14:31:12 +0200

    new change

--- stockManagement/application/views/createInventory.php
+++ stockManagement/application/views/createInventory.php
@@ -2,10 +2,14 @@
 <html lang="en">
 
 <head>
+<?php if (!isset($SESSION['user_data'])){
+        header(base_url().'/User/login');
+    }
+    ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Stock Management - Register User</title>
+    <title>Stock Management - Create Inventory</title>
     <Link rel="stylesheet" href="<?php echo base_url(); ?>/public/css/bootstrap.css">
     <style>
         .loginRegister {
