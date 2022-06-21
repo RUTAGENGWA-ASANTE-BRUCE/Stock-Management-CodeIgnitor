c6c487d0bcedf80c60feeae37790d8a5701a0f86
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-06-21 14:30:18 +0200

    new change

--- stockManagement/application/models/User_model.php
+++ stockManagement/application/models/User_model.php
@@ -13,6 +13,10 @@
         function updateUser($user_id,$formArray){
             $this->db->where('user_id',$user_id);
             $this->db->update('users',$formArray);
+            $this->db->where('user_id',$user_id);
+
+            return $user=$this->db->get('users')->row_array();//select * from users where user_id=?;
+
         }
         function getUser($user_id){
             $this->db->where('user_id',$user_id);
